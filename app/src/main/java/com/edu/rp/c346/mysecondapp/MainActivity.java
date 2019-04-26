afc719bc126cde18ff87030e21d6a8ddf1e6811b
AUNG MOE THET <18045945@rp.edu.sg>
2019-04-26 15:05:10 +0800

    Initial commit

---
+++ app/src/main/java/com/edu/rp/c346/mysecondapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.edu.rp.c346.mysecondapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
